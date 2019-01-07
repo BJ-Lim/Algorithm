64317c7e4600f691ca1ff98db00e4602ffb8fffa
BJ-Lim <fortress0427@gmail.com>
2019-01-07 13:54:47 +0900

    made auto_structure.sh

---
+++ auto_structure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	echo "usage : $0 dirName"
+else
+	mkdir $1
+	cd $1
+	touch $1.c
+
+#makefile
+	echo "all: $1Test" > makefile
+	echo -e "\n$1Test: $1.c $1Test.c" >> makefile
+	echo "	gcc $1Test.c -o $1Test" >> makefile
+	echo -e "\nclean:"	>> makefile
+	echo "	rm $1Test" >> makefile
+
+#Test file
+	echo "#include <stdio.h>" > $1Test.c
+	echo -e "\nint main(int argc, char *argv[]){\n\n}" >> $1Test.c
+fi
