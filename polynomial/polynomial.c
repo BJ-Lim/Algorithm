1aa0dd4d695ce5e9ef8db75fe616c267b963fa5c
BJ-Lim <fortress0427@gmail.com>
2019-01-23 17:00:48 +0900

    completed add_term in polynomial

--- polynomial/polynomial.c
+++ polynomial/polynomial.c
@@ -7,32 +7,33 @@ struct term{
 };
 
 typedef struct term TERM;
+typedef struct term POLY;
 
-void free_poly(TERM **head);
-void init_poly(TERM **head);
-int max_exp(TERM **head);
-void add_term(TERM **head, int coef, int exp);
-void del_term(TERM **head, int coef, int exp);
-void mult_term(TERM **head, int coef, int exp);
-void add_poly(TERM **head1, TERM **head2);
-void mult_poly(TERM **head1, TERM **head2);
+
+void free_poly(POLY **head);						//ok
+void init_poly(POLY **head);						//ok
+int max_exp(POLY **head);							//ok
+void add_term(POLY **head, int coef, int exp);		//ok
+void del_term(POLY **head, int coef, int exp);
+void mult_term(POLY **head, int coef, int exp);
+void add_poly(POLY **head1, TERM **head2);
+void mult_poly(POLY **head1, TERM **head2);
+void print_poly(POLY **head);						//ok
 
 /*
  	head must be initialized as NULL.
  */
-void init_poly(TERM **head){
+void init_poly(POLY **head){
 	TERM *term = (TERM*)malloc(sizeof(TERM));
 	term->coef = 0;
 	term->exp = 0;
 	term->next = NULL;
 
-	if( *head != NULL ){
-		free_poly(head);
-	}
+	free_poly(head);
 	*head = term;
 }
 
-void free_poly(TERM **head){
+void free_poly(POLY **head){
 	TERM * term;
 
 	while( *head != NULL ){
@@ -42,6 +43,47 @@ void free_poly(TERM **head){
 	}
 }
 
-int max_exp(TERM **head){
+int max_exp(POLY **head){
+	return (*head)->exp;
+}
 
+void add_term(POLY **head, int coef, int exp){
+	TERM *c = *head;
+	TERM *term = (TERM*)malloc(sizeof(TERM));
+	term->coef = coef;
+	term->exp = exp;
+	term->next = NULL;
+
+	//front
+	if( max_exp(head) < exp ){
+		term->next = c;
+		*head = term;
+		return;
+	}
+
+	//mid
+	while( c->next ){
+		if( c->exp == exp ){
+			c->coef += coef;
+			return;
+		}
+		if( c->next->exp < exp ){
+			term->next = c->next;
+			c->next = term;
+			return;
+		}
+		c = c->next;
+	}
+	//end
+	c->next = term;
+}
+
+void print_poly(POLY **head){
+	TERM *c = *head;
+
+	while( c->next ){
+		printf("(%d)^%d + ", c->coef, c->exp);
+		c = c->next;
+	}
+	printf("(%d)^%d\n", c->coef, c->exp);
 }
--- polynomial/polynomialTest.c
+++ polynomial/polynomialTest.c
@@ -2,5 +2,23 @@
 #include "polynomial.c"
 
 int main(int argc, char *argv[]){
+	POLY *head = NULL;
+
+	init_poly(&head);
+	print_poly(&head);
+
+	add_term(&head, 3, 4);
+	print_poly(&head);
+
+	add_term(&head, 3, 2);
+	print_poly(&head);
+
+	add_term(&head, 3, 2);
+	print_poly(&head);
+
+	add_term(&head, 3, 6);
+	print_poly(&head);
+
+	free_poly(&head);
 
 }
