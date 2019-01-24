8861d447d456207858b7a59794ca4d5070bbbc93
BJ-Lim <fortress0427@gmail.com>
2019-01-24 16:53:36 +0900

    modified and completed polynomial

--- polynomial/polynomial.c
+++ polynomial/polynomial.c
@@ -1,3 +1,6 @@
+/*
+ 	Last term always exists as 0 coef.
+ */
 #include <stdlib.h>
 
 struct term{
@@ -14,10 +17,10 @@ void free_poly(POLY **head);						//ok
 void init_poly(POLY **head);						//ok
 int max_exp(POLY **head);							//ok
 void add_term(POLY **head, int coef, int exp);		//ok
-void del_term(POLY **head, int coef, int exp);
+void del_term(POLY **head, int exp);
 void mult_term(POLY **head, int coef, int exp);
-void add_poly(POLY **head1, TERM **head2);
-void mult_poly(POLY **head1, TERM **head2);
+void add_poly(POLY **head1, POLY **head2);
+void mult_poly(POLY **head1, POLY **head2);
 void print_poly(POLY **head);						//ok
 
 /*
@@ -74,6 +77,10 @@ void add_term(POLY **head, int coef, int exp){
 		}
 		c = c->next;
 	}
+	if( exp == 0 ){
+		c->coef += coef;
+		return;
+	}
 	//end
 	c->next = term;
 }
@@ -82,8 +89,100 @@ void print_poly(POLY **head){
 	TERM *c = *head;
 
 	while( c->next ){
-		printf("(%d)^%d + ", c->coef, c->exp);
+		printf("(%d)x^%d + ", c->coef, c->exp);
 		c = c->next;
 	}
-	printf("(%d)^%d\n", c->coef, c->exp);
+	printf("(%d)x^%d\n", c->coef, c->exp);
+}
+
+void del_term(POLY **head, int exp){
+	TERM *c = *head;
+	TERM *t = NULL;
+
+	//front
+	if( max_exp(head) == exp ){
+		if( exp == 0 ){
+			c->coef = 0;
+			return;
+		}
+		*head = c->next;
+		free(c);
+		return;
+	}
+
+	while( c->next ){
+		if( c->next->exp == exp ){
+			if( exp == 0 ){
+				c->next->coef = 0;
+				return;
+			}
+			t = c->next;
+			c->next = c->next->next;
+			free(t);
+			return;
+		}
+		c = c->next;
+	}
+}
+
+
+void mult_term(POLY **head, int coef, int exp){
+	TERM *c = *head;
+
+	TERM *z = (TERM*)malloc(sizeof(TERM));
+	z->coef = 0;
+	z->exp = 0;
+	z->next = NULL;
+
+	while( c->next ){
+		c->coef = c->coef * coef;
+		c->exp = c->exp + exp;
+
+		c = c->next;
+	}
+
+	if( c->exp ){
+		c->coef = c->coef * coef;
+		c->exp = c->exp + exp;
+		c->next = z;
+	}
+}
+
+
+void add_poly(POLY **head1, POLY **head2){
+	TERM *c = *head2;
+
+	while(c){
+		add_term(head1, c->coef, c->exp);
+		c = c->next;
+	}
+}
+
+void copy_poly(POLY **dest, POLY **org){
+	TERM *c = *org;
+	init_poly(dest);
+
+	while( c ){
+		add_term(dest, c->coef, c->exp);
+		c = c->next;
+	}
+}
+
+void mult_poly(POLY **head1, POLY **head2){
+	TERM *c = *head2;
+	POLY *rs = NULL;	
+	POLY *t = NULL;		//It'll have a temporal polynomial
+
+	init_poly(&rs);
+
+	while( c && c->coef ){
+		copy_poly(&t, head1);
+		mult_term(&t, c->coef, c->exp);
+		add_poly(&rs, &t);
+		c = c->next;
+	}
+
+	copy_poly(head1, &rs);
+	free_poly(&rs);
+	free_poly(&t);
 }
--- polynomial/polynomialTest.c
+++ polynomial/polynomialTest.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[]){
 	POLY *head = NULL;
+	POLY *head2 = NULL;
 
 	init_poly(&head);
 	print_poly(&head);
@@ -19,6 +20,70 @@ int main(int argc, char *argv[]){
 	add_term(&head, 3, 6);
 	print_poly(&head);
 
+	add_term(&head, 3, 0);
+	print_poly(&head);
+
+	printf("------------------------------------\n");
+
+	del_term(&head, 6);
+	print_poly(&head);
+
+	del_term(&head, 6);
+	print_poly(&head);
+
+	del_term(&head, 4);
+	print_poly(&head);
+
+	del_term(&head, 0);
+	print_poly(&head);
+
+	del_term(&head, 2);
+	print_poly(&head);
+
+	printf("------------------------------------\n");
+
+	add_term(&head, 3, 0);
+	print_poly(&head);
+
+	del_term(&head, 0);
+	print_poly(&head);
+
+	printf("--------------------del test completed!-------------------\n");
+
+	add_term(&head, 1, 1);
+	add_term(&head, 2, 2);
+	add_term(&head, 3, 3);
+
+	mult_term(&head, 2, 2);
+	print_poly(&head);
+
+	printf("--------------------mult term completed!------------------\n");
+
+	init_poly(&head2);
+	add_term(&head2, 1, 1);
+	add_term(&head2, 2, 2);
+	add_term(&head2, 3, 3);
+	printf("[poly1]\n");
+	print_poly(&head);
+	printf("[poly2]\n");
+	print_poly(&head2);
+	printf("[poly1 + poly2]\n");
+	add_poly(&head, &head2);
+	print_poly(&head);
+
+	printf("--------------------add poly completed!------------------\n");
+
+	printf("[poly1]\n");
+	print_poly(&head);
+	printf("[poly2]\n");
+	print_poly(&head2);
+	printf("[poly1 * poly2]\n");
+	mult_poly(&head, &head2);
+	print_poly(&head);
+
+	printf("--------------------mult poly completed!------------------\n");
+
 	free_poly(&head);
+	free_poly(&head2);
 
 }
