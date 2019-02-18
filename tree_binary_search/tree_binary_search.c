bafb2d085619b150e3702423a963efde2280375d
BJ-Lim <fortress0427@gmail.com>
2019-02-18 14:41:02 +0900

    updated tree_binary_search

--- tree_binary_search/tree_binary_search.c
+++ tree_binary_search/tree_binary_search.c
@@ -10,6 +10,9 @@ void createBST(Tree **root);
 Tree* searchBST(Tree **root, int key);
 void insertBST(Tree **root, int key);
 void deleteBST(Tree **root, int key);
+void preorder(Tree *tree);
+void inorder(Tree *tree);
+void postorder(Tree *tree);
 
 void createBST(Tree **root){
 	*root = (Tree*)malloc(sizeof(Tree));
@@ -23,10 +26,12 @@ void createBST(Tree **root){
 		return targeted pointer
 	case2) didn't search target
 		return pointer that has address prior to target
+	case3) root == NULL
+		return NULL
  */
 Tree* searchBST_b(Tree **root, int key){
 	Tree *t = *root;
-	Tree *b = *root;
+	Tree *b = NULL;
 
 	while(t){
 		b = t;
@@ -38,7 +43,6 @@ Tree* searchBST_b(Tree **root, int key){
 		else if( t->key == key )
 			break;
 	}
-
 	return b;
 }
 
@@ -58,6 +62,15 @@ void insertBST(Tree **root, int key){
 	Tree *sub = NULL;
 	Tree *t = searchBST_b(root, key);
 
+	/*
+	 	case) root == NULL
+	 */
+	if( t == NULL ){
+		createBST(root);
+		(*root)->key = key;
+		return;
+	}
+
 	if(t->key == key)		//case) root tree already has the key
 		return;
 
@@ -69,3 +82,68 @@ void insertBST(Tree **root, int key){
 	else if( t->key > key )
 		t->left = sub;
 }
+
+void deleteBST(Tree **root, int key){
+
+}
+
+void print(Tree *tree){
+	printf("%3d", tree->key);
+}
+
+void preorder_r(Tree *tree){
+	if(tree){
+		print(tree);
+		preorder_r(tree->left);
+		preorder_r(tree->right);
+	}
+}
+
+void preorder(Tree *tree){
+	printf("[preorder] : ");
+	preorder_r(tree);
+	printf("\n");
+}
+
+void inorder_r(Tree *tree){
+	if(tree){
+		inorder_r(tree->left);
+		print(tree);
+		inorder_r(tree->right);
+	}
+}
+
+void inorder(Tree *tree){
+	printf("[inorder] : ");
+	inorder_r(tree);
+	printf("\n");
+}
+
+void postorder_r(Tree *tree){
+	if(tree){
+		postorder_r(tree->left);
+		postorder_r(tree->right);
+		print(tree);
+	}
+}
+
+void postorder(Tree *tree){
+	printf("[postorder] : ");
+	postorder_r(tree);
+	printf("\n");
+}
+
+void postfree_r(Tree *tree){
+	if(tree){
+		postfree_r(tree->left);
+		postfree_r(tree->right);
+		free(tree);
+	}
+}
+
+void postfree(Tree **root){
+	postfree_r(*root);
+	*root = NULL;
+}
+
+
--- tree_binary_search/tree_binary_searchTest.c
+++ tree_binary_search/tree_binary_searchTest.c
@@ -2,5 +2,26 @@
 #include "tree_binary_search.c"
 
 int main(int argc, char *argv[]){
+	Tree *root = NULL;
+
+	insertBST(&root, 60);
+	insertBST(&root, 40);
+	insertBST(&root, 80);
+	insertBST(&root, 30);
+	insertBST(&root, 50);
+	insertBST(&root, 70);
+	insertBST(&root, 90);
+	insertBST(&root, 45);
+	insertBST(&root, 55);
+	insertBST(&root, 85);
+	insertBST(&root, 95);
+
+
+	preorder(root);
+	inorder(root);
+	postorder(root);
+
+	postfree(&root);
+	postorder(root);
 
 }
